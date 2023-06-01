0b7fd3179afc51b882410bc570ec915aecfcfa29
sophiazoi <133604009+sophiazoi@users.noreply.github.com>
2023-06-01 09:49:57 +0200

    Basic Exercises

---
+++ Basic/Exercise2-3.js
@@ -0,0 +1,3 @@
+console.log("Hi my name is Martin");
+var age = 25;
+console.log("Hi my name is Martin and I am " + age + " years old");
---
+++ Basic/Exercise4.js
@@ -0,0 +1,4 @@
+var players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+
+var thirdPlayer = players[2];
+console.log("The most valuable player of the match is " + thirdPlayer);
